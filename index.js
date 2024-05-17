00cede7b13960dc7e545a4bd36c7bd7d91b30025
Janbolot <janbolotcode@gmail.xom>
2024-05-16 19:06:52 -0700

    refresh

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 
 import router from "./router/index.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const app = express();
 
 app.use(cors());
